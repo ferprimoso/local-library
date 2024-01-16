30d380cc1a104962388ce9d64c4af4eaed5970ef
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2024-01-16 04:05:49 -0300

    Create Models and populateDb

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+// Mongoose Connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDb = 'mongodb+srv://admin:t1AyzWXubnzVstyU@cluster0.ui4aitj.mongodb.net/local_library?retryWrites=true&w=majority';
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDb)
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
