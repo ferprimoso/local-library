aa12721b83b1a12647ad92deb877f84a0912086b
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2024-01-17 14:05:23 -0300

    Implement view pages and controller GET logic

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -26,6 +27,17 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
+
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifespan = '('
+  this.date_of_birth ? lifespan += DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : lifespan += '';
+  lifespan += ' - '
+  this.date_of_death ? lifespan += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : lifespan += '';
+  lifespan += ')'
+
+  return lifespan
+});
+
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
